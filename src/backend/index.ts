5a36d1a1ec19b59814ed91bb77f2801d8ed3b962
Flor_Zamorano <108295543+FlorZ5@users.noreply.github.com>
2024-02-25 15:24:59 -0600

    Update index.ts

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -48,7 +48,58 @@ export default Server(() => {
             return res.json(paquetes);
         }
     });
-    
+
+    app.get("/paquetes/:id", (req, res) => {
+        if (paquetes.length === 0) {
+            return res.status(404).json({ error: 'No packages register found.' });
+        }
+        else
+        {            
+        const id = req.params.id;
+        const paqueteBusqueda = paquetes.find((paquete) => paquete.id === id);
+        if (!paqueteBusqueda) {
+            return res.status(404).send("No se encontró ningún registro con el ID " + id);
+        }
+        else{
+            return res.json(paqueteBusqueda);
+        }
+        }
+    });
+
+    app.get("/paquetesn/:nombre", (req, res) => {
+        if (paquetes.length === 0) {
+            return res.status(404).json({ error: 'No packages register found.' });
+        }
+        else
+        {
+            const nombre = req.params.nombre;
+        const paqueteBusquedaN = paquetes.find((paquete) => paquete.nombre === nombre);
+        if (!paqueteBusquedaN) {
+            return res.status(404).send("No se encontró ningún registro con el nombre " + nombre);
+        }
+        else{
+            return res.json(paqueteBusquedaN);
+        }
+        }
+    });
+
+    app.get("/paquetesg/:guiaSeguimiento", (req, res) => {
+        if (paquetes.length === 0) {
+            return res.status(404).json({ error: 'No packages register found.' });
+        }
+        else
+        {
+            const guiaSeguimiento = req.params.guiaSeguimiento;
+        const paqueteBusquedaG = paquetes.find((paquete) => paquete.guiaSeguimiento === guiaSeguimiento);
+        if (!paqueteBusquedaG) {
+            return res.status(404).send("No se encontró ningún registro con la Guía de seguimiento " + guiaSeguimiento);
+        }
+        else{
+            return res.json(paqueteBusquedaG);
+        }
+        }
+    });
+
     app.post("/paquetes", (req, res) => {
         if (req.body.id) {
             return res.status(400).json({
@@ -156,8 +207,66 @@ export default Server(() => {
         res.send("El registro " + id + " se ha actualizado correctamente");
     });
     
+    app.put("/paquetesg/:guiaSeguimiento", (req, res) => {
+        const guiaSeguimiento = req.params.guiaSeguimiento;
+        const paquete = paquetes.find((paquete) => paquete.guiaSeguimiento === guiaSeguimiento);
+    
+        if (!paquete) {
+            return res.status(404).send("Paquete con la Guía de seguimiento" + guiaSeguimiento + " no encontrado.");
+        }
+    
+        const validFields = ['nombre', 'apellidos', 'partida', 'tipo', 'peso', 'receptorNombre', 'receptorApellidos', 'destino', 'estado', 'ubicacion'];
+    
+        if (req.body.id) {
+            return res.status(400).json({
+                error: 'El valor ID no puede ser actualizado manualmente.' 
+            });
+        }
+    
+        if (req.body.guiaSeguimiento) {
+            return res.status(400).json({
+                error: 'La guía de seguimiento no puede ser actualizado manualmente.'
+             });
+        }
+
+        if (req.body.precio) {
+            return res.status(400).json({
+                error: 'El precio del envío no puede ser actualizado manualmente.'
+            });
+        }
+    
+        for (let key in req.body) {
+            if (!validFields.includes(key)) {
+                return res.status(400).json({ error: 'El campo ' + key + ' no es válido.' });
+            }
+    
+            if (!req.body[key]) {
+                return res.status(400).json({ error: 'El campo ' + key + ' no puede estar vacío.' });
+            }
+        }
+    
+        if (req.body.peso) {
+            const peso = parseFloat(req.body.peso);
+            const precio = (peso * 25).toString();
+    
+            req.body.peso = peso;
+            req.body.precio = precio;
+        }
+    
+        const updatedPaquete = { ...paquete, ...req.body };
+    
+        paquetes = paquetes.map((b) => b.id === updatedPaquete.guiaSeguimiento ? updatedPaquete : b);
+    
+        res.send("El registro con la Guía de seguimiento" + guiaSeguimiento + " se ha actualizado correctamente");
+    });
+
     app.delete("/paquetes/:id", (req, res) => {
-        const id = req.params.id;
+        if (paquetes.length === 0) {
+            return res.status(404).json({ error: 'No packages register found.' });
+        } 
+        else
+        {
+            const id = req.params.id;
         const paqueteIndex = paquetes.findIndex((paquete) => paquete.id === id);
     
         if (paqueteIndex === -1) {
@@ -166,7 +275,44 @@ export default Server(() => {
     
         paquetes.splice(paqueteIndex, 1);
         res.send("El registro " + id + " ha sido eliminado correctamente.");
+        }
     });
+
+    app.delete("/paquetesg/:guiaSeguimiento", (req, res) => {
+        if (paquetes.length === 0) {
+            return res.status(404).json({ error: 'No packages register found.' });
+        }
+        else
+        {
+            const guiaSeguimiento = req.params.guiaSeguimiento;
+            const paqueteIndex = paquetes.findIndex((paquete) => paquete.guiaSeguimiento === guiaSeguimiento);
+        
+            if (paqueteIndex === -1) {
+                return res.status(404).send("No se encontró ningún registro con la Guía de seguimiento " + guiaSeguimiento);
+            }
+        
+            paquetes.splice(paqueteIndex, 1);
+            res.send("El registro con la Guía de seguimiento" + guiaSeguimiento + " ha sido eliminado correctamente.");
+        }
+           });
+
+    app.delete("/paquetesn/:nombre", (req, res) => {
+        if (paquetes.length === 0) {
+            return res.status(404).json({ error: 'No packages register found.' });
+        }
+        else
+        {
+            const nombre = req.params.nombre;
+        const paqueteIndex = paquetes.findIndex((paquete) => paquete.nombre === nombre);
     
+        if (paqueteIndex === -1) {
+            return res.status(404).send("No se encontró ningún registro con el nombre " + nombre);
+        }
+    
+        paquetes.splice(paqueteIndex, 1);
+        res.send("El registro con el Nombre" + nombre + " ha sido eliminado correctamente.");
+        }  
+    });
+
     return app.listen();
 });
